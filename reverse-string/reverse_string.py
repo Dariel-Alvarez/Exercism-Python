5301c8f53588efb58fe407086ad55153d509f532
Dariel-Alvarez <darieleaj@gmail.com>
2025-01-30 00:10:31 -0400

    Solution to Exercise #28 Reverse String

---
+++ reverse-string/reverse_string.py
@@ -0,0 +1,9 @@
+def reverse(text):
+    return text[::-1]
+
+    #Alternate Solution:
+    # reverse_text = ""
+    # for _ in text:
+    #     reverse_text += text[-1]
+    #     text = text[0:-1]
+    # return reverse_text
